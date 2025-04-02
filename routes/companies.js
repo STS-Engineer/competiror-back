a55489fea34065e094cd6df4ecaf9fbd0ad605a3
STS-Engineer <administration.sts@avocarbon.com>
2025-04-03 00:01:18 +0100

    Update companies.js

--- routes/companies.js
+++ routes/companies.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   const { 
     name, email, headquarters_location, r_and_d_location, country, product, employeestrength, revenues, 
-    telephone, website, productionvolumes, keycustomers, region, foundingyear, fyear
+    telephone, website, productionvolumes, keycustomers, region, foundingyear, fyear, keymanagement
   } = req.body;
 
   try {
@@ -33,13 +33,13 @@ router.post('/', async (req, res) => {
     const result = await pool.query(
       `INSERT INTO companies (
         name, email, headquarters_location, r_and_d_location, country, product, employeestrength, revenues, 
-        telephone, website, productionvolumes, keycustomers, region, foundingyear, fyear
+        telephone, website, productionvolumes, keycustomers, region, foundingyear, fyear, keymanagement
       ) VALUES (
-        $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15
+        $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, , $16
       ) RETURNING *`,
       [
         name, email, headquarters_location, r_and_d_location, country, productsJSON, employeestrength, revenues, 
-        telephone, website, productionvolumes, keycustomers, region, foundingYear, fYear
+        telephone, website, productionvolumes, keycustomers, region, foundingYear, fYear, keymanagement
       ]
     );
 
